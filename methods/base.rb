1527e27ccbc99844b4d9df9f622aea23cf126874
Koten <koten0224@gmail.com>
2020-08-29 18:12:28 +0800

    fix(base): include all new modules

--- methods/base.rb
+++ methods/base.rb
@@ -1,11 +1,17 @@
+require './methods/comparison'
 require './methods/definers'
-require './methods/style'
 require './methods/implementation'
+require './methods/iteration'
+require './methods/standard'
+require './methods/style'
 module Methods
   module Base
     def self.included(base)
+      base.include Comparison
       base.include Definers
       base.include Implementation
+      base.include Iteration
+      base.include Standard
       base.include Style
     end
   end
