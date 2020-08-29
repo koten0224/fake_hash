1d6b1178f62dbc4c708585e849a98766561a5921
Koten <koten0224@gmail.com>
2020-08-29 16:17:47 +0800

    feat(refactor): set up the methods to multi modules

--- fake_hash.rb
+++ fake_hash.rb
@@ -1,4 +1,6 @@
+require './methods/base'
 class FakeHash
+  include Methods::Base
   attr_reader :keys
 
   def initialize(default=nil, &block)
@@ -8,51 +10,5 @@ class FakeHash
     else
       @default = default
     end
-  end
-
-  def [](key)
-    return inst_get(key) if keys.include? key
-    if @default.is_a? Proc
-      self[key] = @default.call
-    elsif @default != nil
-      self[key] = @default
-    else
-      inst_get(key)
-    end
-  end
-
-  def []=(key, value)
-    keys << key unless keys.include? key
-    inst_set(key, value)
-  end
-
-  def to_s
-    str = '{'
-    keys.each_with_index do |key, index|
-      str += key.inspect + '=' + inst_get(key).inspect
-      if index < keys.length - 1
-        str += ', '
-      end
-    end
-    str += '}'
-    return str
-  end
-
-  def inspect
-    to_s
-  end
-
-  private
-
-  def trans_key(key)
-    "@_#{key.class}_#{key}"
-  end
-
-  def inst_set(key, value)
-    instance_variable_set(trans_key(key), value)
-  end
-  
-  def inst_get(key)
-    instance_variable_get(trans_key(key))
   end
 end
---
+++ methods/implementation.rb
@@ -0,0 +1,17 @@
+module Methods
+  module Implementation
+    def [](key)
+      return inst_get(key) if keys.include? key
+      if @default.is_a? Proc
+        self[key] = @default.call
+      elsif @default != nil
+        self[key] = @default
+      end
+    end
+
+    def []=(key, value)
+      keys << key unless keys.include? key
+      inst_set(key, value)
+    end
+  end
+end
