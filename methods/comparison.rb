fddb474135553b04a7a2359c3d7fad9531919c8d
Koten <koten0224@gmail.com>
2020-08-29 18:12:12 +0800

    feat(comparison): add comparison methods

---
+++ methods/comparison.rb
@@ -0,0 +1,39 @@
+module Methods
+  module Comparison
+    def ==(other)
+      return false unless same_class?(other)
+      other.each do |key, value|
+        return false if self[key] != value
+      end
+      true
+    end
+    
+    def >(other)
+      return false unless same_class?(other)
+      other.all?{|key,value|keys.include?(key) && self[key]==value}
+    end
+    
+    def <(other)
+      return false unless same_class?(other)
+      all?{|key,value|other.keys.include?(key) && other[key]==value}
+    end
+    
+    def >=(other)
+      self > other || self == other
+    end
+    
+    def <=(other)
+      self < other || self == other
+    end
+    
+    def !=(other)
+      !(self == other)
+    end
+
+    private
+
+    def same_class?(other)
+      other.is_a?(FakeHash) || other.is_a?(Hash)
+    end
+  end
+end
